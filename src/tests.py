26a41250058c714a3939dc0af4003975b6d057ea
Russell <russell@example.org>
2016-04-08 15:12:23 +0100

    Update to tests.py

--- src/tests.py
+++ src/tests.py
@@ -4,16 +4,36 @@ import tempfile
 from enchant.checker import SpellChecker
 from enchant.tokenize import EmailFilter, URLFilter
 from funct import checkline
+from funct import filechecker
 
 class TestFuncts(unittest.TestCase):
 
     def test_checkline(self):
         checker = SpellChecker("en_GB", filters=[URLFilter, EmailFilter])
         pwl = enchant.request_pwl_dict('dict.txt')
-        tfile = tempfile.TemporaryFile()
+        with open('test.txt', 'w+') as tfile:
+            self.assertEqual(checkline('Lots of words that are spelt orrectly!', 'filename.txt', False, checker, pwl, tfile), 1)
 
-        self.assertEqual(checkline('Lots of words that are spelt correctly!', 'filename.txt', False, checker, pwl, tfile), 0)
 
+    def test_checkline1(self):
+        checker = SpellChecker("en_GB", filters=[URLFilter, EmailFilter])
+        pwl = enchant.request_pwl_dict('dict.txt')
+        with open('test.txt', 'w+') as tfile:
+            self.assertEqual(checkline('Lots of words that are spelt correctly!', 'filename.txt', True, checker, pwl, tfile), 0)
+
+
+    def test_checkline2(self):
+        checker = SpellChecker("en_GB", filters=[URLFilter, EmailFilter])
+        pwl = enchant.request_pwl_dict('dict.txt')
+        with open('test.txt', 'w+') as tfile:
+            self.assertEqual(checkline('Lots of words that are spelt correctly!', 'filename.txt', False, checker, pwl, tfile), 0)
+
+
+    def test_checkline3(self):
+        checker = SpellChecker("en_GB", filters=[URLFilter, EmailFilter])
+        pwl = enchant.request_pwl_dict('dict.txt')
+        with open('test.txt', 'w+') as tfile:
+            self.assertEqual(checkline('Lots of words that are spelt orrectly!', 'filename.txt', True, checker, pwl, tfile), 0)
 
 if __name__ == '__main__':
     unittest.main()
