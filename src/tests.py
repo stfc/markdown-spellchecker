f13a0135c3b5b85ff1ba5e75c78f6e3445d6695c
James Adams <james@bluezen.co.uk>
2016-04-26 13:09:16 +0100

    Update tests to creeate object correctly

--- src/tests.py
+++ src/tests.py
@@ -1,36 +1,30 @@
 import unittest
 import enchant
-from enchant.checker import SpellChecker
-from enchant.tokenize import EmailFilter, URLFilter
 from markspelling import MarkSpelling
 
 class TestFuncts(unittest.TestCase):
 
     def test_checkline_no_errors(self):
-        checker = SpellChecker("en_GB", filters=[URLFilter, EmailFilter])
         pwl = enchant.request_pwl_dict('dict.txt')
-        markspell = MarkSpelling('dummy', checker, pwl)
+        markspell = MarkSpelling(pwl)
         self.assertEqual(markspell.checkline('Lots of words that are spelt correctly!', 'filename.txt', False), 0)
 
 
     def test_checkline_one_error(self):
-        checker = SpellChecker("en_GB", filters=[URLFilter, EmailFilter])
         pwl = enchant.request_pwl_dict('dict.txt')
-        markspell = MarkSpelling('dummy', checker, pwl)
+        markspell = MarkSpelling(pwl)
         self.assertEqual(markspell.checkline('Lots of words that are spelt orrectly!', 'filename.txt', False), 1)
 
 
     def test_checkline_code_block_good(self):
-        checker = SpellChecker("en_GB", filters=[URLFilter, EmailFilter])
         pwl = enchant.request_pwl_dict('dict.txt')
-        markspell = MarkSpelling('dummy', checker, pwl)
+        markspell = MarkSpelling(pwl)
         self.assertEqual(markspell.checkline('Lots of words that are spelt correctly!', 'filename.txt', True), 0)
 
 
     def test_checkline_code_block_mistake(self):
-        checker = SpellChecker("en_GB", filters=[URLFilter, EmailFilter])
         pwl = enchant.request_pwl_dict('dict.txt')
-        markspell = MarkSpelling('dummy', checker, pwl)
+        markspell = MarkSpelling(pwl)
         self.assertEqual(markspell.checkline('Lots of words that are spelt orrectly!', 'filename.txt', True), 0)
 
 if __name__ == '__main__':
