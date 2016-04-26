3e90a4961e4e42da2d309c7eab2fe522770792f5
James Adams <james@bluezen.co.uk>
2016-04-26 12:23:35 +0100

    Overhaul loading of configurables
    Also input checking

--- src/spellchecker.py
+++ src/spellchecker.py
@@ -9,28 +9,33 @@ import logging
 from markspelling import MarkSpelling
 
 
-def errortotalfunct(errortotal, errortotalprev, filename_jsonscore):
+def errortotalfunct(errortotal, errortotalprev, file_state):
     logger = logging.getLogger('markdown-spellchecker')
     logger.info('Errors in total: %d', errortotal)
     if errortotal <= errortotalprev:
         logger.info('Pass. you scored better or equal to the last check')
-        with open(filename_jsonscore, 'w') as outfile:
+        with open(file_state, 'w') as outfile:
             json.dump(errortotal, outfile)
             return True
     else:
         logger.error('Fail. try harder next time')
-        with open(filename_jsonscore, 'w') as outfile:
+        with open(file_state, 'w') as outfile:
             # saves errortotal to json file for future use
             json.dump(errortotal, outfile)
             return False
 
 
-def main():
-    directory_self = os.path.dirname(os.path.realpath(__file__))
-    directory_root = os.path.dirname(directory_self)
+def abspath(path):
+    """Return an absolute form of path relative to this script"""
+    root = os.path.dirname(os.path.realpath(__file__))
+    if not os.path.isabs(path):
+        path = os.path.join(root, path)
+    return path
 
+
+def main():
     config = configparser.ConfigParser()
-    config.read(os.path.join(directory_self, 'config.ini'))
+    config.read(abspath('config.ini'))
 
     logger = logging.getLogger('markdown-spellchecker')
 
@@ -39,39 +44,39 @@ def main():
         logger.setLevel = logging.DEBUG
 
     if config.getboolean('DEFAULT', 'log_to_file'):
-        logging.basicConfig(filename=os.path.join(directory_self, 'spellchecker.log'))
+        logging.basicConfig(filename=abspath('spellchecker.log'))
 
-    directory_posts = os.path.join(directory_root, config.get('DEFAULT', 'directory_source'))
-    if os.listdir(directory_posts) == []:
+    directory_source = abspath(config.get('DEFAULT', 'directory_source'))
+    file_state = abspath(config.get('DEFAULT', 'file_state'))
+    personal_word_list = abspath(config.get('DEFAULT', 'personal_word_list'))
+
+    if not os.path.exists(directory_source):
+        logger.error('Source directory "%s" does not exist', directory_source)
+        sys.exit(1)
+
+    if os.listdir(directory_source) == []:
         logger.error('No .md files to evaluate')
-
-    filename_jsonscore = config.get('DEFAULT', 'file_state')
-    if not os.path.isabs(filename_jsonscore):
-        filename_jsonscore = os.path.join(directory_self, filename_jsonscore)
-
-    filename_pwl = config['DEFAULT']['personal_word_list']
-    if not os.path.isabs(filename_pwl):
-        filename_pwl = os.path.join(directory_self, filename_pwl)
+        sys.exit(1)
 
     pwl = None
-    if os.path.exists(filename_pwl):
+    if os.path.exists(personal_word_list):
         logger.debug("PWL file exists")
-        pwl = enchant.request_pwl_dict(filename_pwl)
+        pwl = enchant.request_pwl_dict(personal_word_list)
     else:
         logger.error("PWL file does not exist")
-        sys.exit(2)
+        sys.exit(1)
 
-    filenameslist = glob.glob(os.path.join(directory_posts, "*.md"))
+    filenameslist = glob.glob(os.path.join(directory_source, "*.md"))
 
     errortotalprev = 0
     try:
-        with open(filename_jsonscore, 'r') as scorefile:
+        with open(file_state, 'r') as scorefile:
             errortotalprev = json.load(scorefile)
     except FileNotFoundError:
-        logger.warning('JSON score file "%s" was not found', filename_jsonscore)
+        logger.warning('JSON score file "%s" was not found', file_state)
     mspell = MarkSpelling(pwl, config.get('DEFAULT', 'spelling_language'), errortotalprev)
     errortotal = mspell.checkfilelist(filenameslist)
-    passed = errortotalfunct(errortotal, errortotalprev, filename_jsonscore)
+    passed = errortotalfunct(errortotal, errortotalprev, file_state)
     if not passed:
         sys.exit(1)
 
