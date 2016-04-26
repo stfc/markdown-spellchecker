62bc3ece76884594bd4fc43bedbe9b36e318590f
James Adams <james@bluezen.co.uk>
2016-04-26 14:36:30 +0100

    PEP8 tweaks

--- src/tests.py
+++ src/tests.py
@@ -2,9 +2,12 @@ import unittest
 from spellchecker import abspath
 from markspelling import MarkSpelling
 
+
 class TestFuncts(unittest.TestCase):
+    """Test cases for spellchecker and markspell"""
 
     def setUp(self):
+        """Create a shared markspell instance to use for testing"""
         self.markspell = MarkSpelling(None)
 
     def test_abspath(self):
@@ -19,54 +22,44 @@ class TestFuncts(unittest.TestCase):
         self.assertTrue(absolute.endswith('/relative'))
         self.assertTrue(absolute.startswith('/'))
 
-
     def test_checkline_no_errors(self):
         """Correctly spelt lines should return no errors"""
         self.assertEqual(self.markspell.checkline('Lots of words that are spelt correctly!', 'filename.txt', False), 0)
-
 
     def test_checkline_one_error(self):
         """Test line with a single spelling error"""
         self.assertEqual(self.markspell.checkline('One word that is spelt icnorrectly!', 'filename.txt', False), 1)
 
-
     def test_checkline_multi_error(self):
         """Test line with a multiple spelling errors"""
         self.assertEqual(self.markspell.checkline('Lts of wrods thta are splet icnorrectly!', 'filename.txt', False), 5)
-
 
     def test_checkline_code_block_good(self):
         """Test line with no spelling errors inside a block of code"""
         self.assertEqual(self.markspell.checkline('Lots of words that are spelt correctly!', 'filename.txt', True), 0)
 
-
     def test_checkline_code_block_mistake(self):
         """Test that spelling errors are ignored inside a block of code"""
         self.assertEqual(self.markspell.checkline('Lots of wrods that are spelt icnorrectly!', 'filename.txt', True), 0)
 
-
     def test_checkline_backtick_good(self):
         """Test that spelling errors are not flagged by inline code snippets"""
         self.assertEqual(self.markspell.checkline('This is an example of `code within backticks`', 'filename.txt', False), 0)
-
 
     def test_checkline_backtick_mistake(self):
         """Test that spelling errors are ignored within inline code snippets"""
         self.assertEqual(self.markspell.checkline('This is a example of `typso niside backtciks`', 'filename.txt', False), 0)
         self.assertEqual(self.markspell.checkline('Outside `backtciks` speeling is still improtant', 'filename.txt', False), 2)
 
-
     def test_checkline_html_good(self):
         """Test that spelling errors are not flagged by inline HTML"""
         self.assertEqual(self.markspell.checkline('Test some <strong>in-line HTML</strong>', 'filename.txt', False), 0)
         self.assertEqual(self.markspell.checkline('Check <i>spelling witihn in-line HTML</i>', 'filename.txt', False), 1)
 
-
     def test_checkline_html_mistake(self):
         """Test that spelling errors are ignored within inline HTML"""
         self.assertEqual(self.markspell.checkline('Ignore <asd>bad tags</fgh>', 'filename.txt', False), 0)
         self.assertEqual(self.markspell.checkline('Evrething <qwe>esle mattters</rty>', 'filename.txt', False), 3)
-
 
 if __name__ == '__main__':
     unittest.main()
